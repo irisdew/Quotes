2411fcd069710d2b33ca6434c937b5713df6e314
marxl <isl9702@naver.com>
2021-04-22 23:42:52 +0900

    Add Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
-      <AppRouter />
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; twttest {new Date().getFullYear()}</footer>
     </>
   );
 }
