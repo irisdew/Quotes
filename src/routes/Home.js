392dc8e5a2746568c1b8330e52744aba2832d58e
marxl <isl9702@naver.com>
2021-04-25 22:08:13 +0900

    Implement image upload feature

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState("");
   useEffect(() => {
     // snapshot gives you update when changes happened in realtime
     dbService.collection("tweets").onSnapshot((snapshot) => {
@@ -31,12 +32,34 @@ const Home = ({ userObj }) => {
     } = event;
     setTweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachement = () => setAttachment("");
   return (
     <div>
       <span>Home</span>
       <form onSubmit={onSubmit}>
         <input value={tweet} onChange={onChange} type="text" placeholder="Whant's on your mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Tweet" />
+        {attachment && (
+          <div>
+            <img alt="attachment" src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachement}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {tweets.map((tweet) => (
