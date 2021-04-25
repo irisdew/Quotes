949e06c13eeb830c36318bf85e782d9d3f7afa4c
marxl <isl9702@naver.com>
2021-04-25 17:46:49 +0900

    Implement delete tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,19 @@
+import { dbService } from "fbase";
 import React from "react";
 
 const Tweet = ({ tweetObj, isAuthor }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this tweet?");
+    if (ok) {
+      await dbService.doc(`tweets/${tweetObj.id}`).delete();
+    }
+  };
   return (
     <div>
       <h4>{tweetObj.text}</h4>
       {isAuthor && (
         <>
-          <button>Delete</button>
+          <button onClick={onDeleteClick}>Delete</button>
           <button>Edit</button>
         </>
       )}
