c4bc68a65244a649839f9ab746d74874945b42d0
marxl <isl9702@naver.com>
2021-04-25 18:28:32 +0900

    Re-create IsloggedIn status

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,22 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
+        setIsLoggedIn(true);
         setUserObj(user);
+      } else {
+        setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
       <footer>&copy; twttest {new Date().getFullYear()}</footer>
     </>
   );
