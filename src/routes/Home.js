c2580733b106beb5d84558ee702595bb0af7b824
marxl <isl9702@naver.com>
2021-04-25 15:47:42 +0900

    Add firestore and add new tweet on firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,29 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("tweets").add({
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
   return (
     <div>
       <span>Home</span>
+      <form>
+        <input value={tweet} onChange={onChange} type="text" placeholder="Whant's on your mind?" maxLength={120} />
+        <input type="submit" value="Tweet" onClick={onSubmit} />
+      </form>
     </div>
   );
 };
